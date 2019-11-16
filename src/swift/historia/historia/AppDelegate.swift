56cfc21a75b1703c8aee4f303540341caa850959
jTitor <me@me.me>
2019-11-15 23:07:50 -0800

    src/swift: add project for macOS app

---
+++ src/swift/historia/historia/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  historia
+//
+//  Created by patuxent on 11/15/19.
+//  Copyright © 2019 jtitor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ src/swift/historia/historia/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  historia
+//
+//  Created by patuxent on 11/15/19.
+//  Copyright © 2019 jtitor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
